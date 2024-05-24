54086b1057e2645de8fe0ef95bc618acdbdb9599
Denis <Denis.kachalov.1997@gmail.com>
2024-05-24 16:39:14 +0400

    added draft code

--- parse.py
+++ parse.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 def get_info(filename):
-    pass
+    res = []
+    with open(filename, "r", encoding = "utf-8") as file_:
+        try:
+            for line in file_:
+                name, login = line.split(";")
+                name = name.strip()
+                login = login.strip()
+                res.append({
+                    "name": name,
+                    "login": login, 
+                })
+        except ValueError as err:
+            raise "incorrect file"
+    return res
 
 def main():
     filename="data/data.txt"
     return get_info(filename)
 
 if __name__ == "__main__":
-    main()
+   print(main())
